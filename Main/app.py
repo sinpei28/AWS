6a24101e9805442b0aac97d22c78a3be41a0c14c
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2022-09-22 01:48:00 +0800

    fixing error

--- Main/app.py
+++ Main/app.py
@@ -1,17 +1,16 @@
-from codecs import getencoder
 from flask import Flask, render_template, request
 from pymysql import connections
 from datetime import date
 
 app = Flask(__name__)
 
-db_conn = connections.Connection(
-    host='hr-database.cleurfoto8r2.us-east-1.rds.amazonaws.com',
-    port=3306,
-    user='main',
-    password='lab-password',
-    db = 'HR'
-)
+# db_conn = connections.Connection(
+#     host='hr-database.cleurfoto8r2.us-east-1.rds.amazonaws.com',
+#     port=3306,
+#     user='main',
+#     password='lab-password',
+#     db = 'HR'
+# )
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -25,28 +24,28 @@ def home():
 # for x in cursor:
 #     print(x)
 
-@app.route("/add_employees", methods=['POST'])
-def AddEmp():
-    firstName = request.form['firstName']
-    lastName = request.form['last_name']
-    email = request.form['email']
-    currentAddress = request.form['currentAddress']
-    phoneNumber = request.form['phoneNumber']
-    emergencyContactNumber = request.form['emergencyContactNumber']
-    gender =  request.form['gender']
-    dob = request.form['dob']
-    department = request.form['department']
+# @app.route("/add_employees", methods=['POST'])
+# def AddEmp():
+#     firstName = request.form['firstName']
+#     lastName = request.form['last_name']
+#     email = request.form['email']
+#     currentAddress = request.form['currentAddress']
+#     phoneNumber = request.form['phoneNumber']
+#     emergencyContactNumber = request.form['emergencyContactNumber']
+#     gender =  request.form['gender']
+#     dob = request.form['dob']
+#     department = request.form['department']
 
-    print(firstName, lastName, email, currentAddress, phoneNumber, emergencyContactNumber, gender, dob, department)
+#     print(firstName, lastName, email, currentAddress, phoneNumber, emergencyContactNumber, gender, dob, department)
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-    print(insert_sql)
+#     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+#     cursor = db_conn.cursor()
+#     print(insert_sql)
 
-    # cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-    # db_conn.commit()
+#     # cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+#     # db_conn.commit()
 
-    return render_template('Payroll.html')
+#     return render_template('Payroll.html')
 
 
 # cursor.execute("create database HRsystem")

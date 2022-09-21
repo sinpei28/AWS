f8f167d301dd1024d23a060b924ecf1e5838ebb1
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2022-09-22 00:14:15 +0800

    add name in index.html

--- Main/app.py
+++ Main/app.py
@@ -1,3 +1,4 @@
+from codecs import getencoder
 from flask import Flask, render_template, request
 from pymysql import connections
 from datetime import date
@@ -12,17 +13,38 @@ db_conn = connections.Connection(
     db = 'HR'
 )
 
-cursor = db_conn.cursor()
-
-# createTableEmployees = "CREATE TABLE Employees (firstName VARCHAR(10),lastName VARCHAR(10), email VARCHAR(15), address VARCHAR(30), phoneNumber VARCHAR(15), emergencyPhoneNumber VARCHAR(15), gender VARCHAR(10), dateOfBirth DATE, department VARCHAR(10), primary key (firstName))"
-# cursor.execute(createTableEmployees)
-cursor.execute("SHOW TABLES")
-for x in cursor:
-    print(x)
-
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
+
+# cursor = db_conn.cursor()
+
+# createTableEmployees = "CREATE TABLE Employees (firstName VARCHAR(10),lastName VARCHAR(10), email VARCHAR(15), address VARCHAR(30), phoneNumber VARCHAR(15), emergencyPhoneNumber VARCHAR(15), gender VARCHAR(10), dateOfBirth DATE, department VARCHAR(10), primary key (firstName))"
+# cursor.execute(createTableEmployees)
+# cursor.execute("SHOW TABLES")
+# for x in cursor:
+#     print(x)
+
+@app.route("/add_employees", methods=['POST'])
+def AddEmp():
+    firstName = request.form['firstName']
+    lastName = request.form['last_name']
+    email = request.form['email']
+    currentAddress = request.form['currentAddress']
+    phoneNumber = request.form['phoneNumber']
+    emergencyContactNumber = request.form['emergencyContactNumber']
+    gender =  request.form['gender']
+    department = request.form['department']
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+    print(insert_sql)
+
+    # cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+    # db_conn.commit()
+
+    # return render_template('AddEmpOutput.html', name=emp_name)
+
 
 # cursor.execute("create database HRsystem")
 # print(cursor)
